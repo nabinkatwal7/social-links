14de107f6f870090832c0a195aef99cd8af88c8d
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-01-19 14:12:21 +0545

    add alt text image

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="bg-[#141414] min-h-screen min-w-screen flex justify-center items-center content-center" >
       <div className={"text-white bg-[#1F1F1F] lg:w-[30%] rounded-xl justify-center p-8 flex flex-col gap-4"} >
           <div className={"w-full flex flex-col gap-2 justify-center items-center"}>
-              <img src="https://randomuser.me/api/portraits/men/71.jpg" className="w-1/3 rounded-full"/>
+              <img src="https://randomuser.me/api/portraits/men/71.jpg" className="w-1/3 rounded-full" alt={"profile"}/>
               <h1 className={"text-3xl"}>Nabin Katwal</h1>
               <p className={"font-bold text-[#BFDB6F]"} >Kathmandu, Nepal</p>
               <blockquote>"Frontend developer and avid reader."</blockquote>
